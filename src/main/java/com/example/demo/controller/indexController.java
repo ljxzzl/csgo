73c59500a73072f160039fb333eef7b461634a6c
zutlh <liuhao19971212@163.com>
2021-05-19 19:57:53 +0800

    update

--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class indexController {
 
-    @RequestMapping("/callback")
+    @GetMapping("/callback")
     public String callback(HttpServletRequest request,
                            HttpServletResponse response) {
-        return "product-center.html";
+        return "redirect:/";
 
     }
 }
